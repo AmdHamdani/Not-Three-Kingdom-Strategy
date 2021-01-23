9155611a2064faed7c1c1d54ecabb6c79feff0bc
AmdHamdani <Hamdaniryuuken@gmail.com>
2021-01-23 14:30:47 +0700

    Add login view

---
+++ Assets/Scripts/Login/LoginView.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginView : MonoBehaviour
+{
+    [SerializeField] private InputField emailField;
+    [SerializeField] private InputField passwordField;
+    [SerializeField] private Button loginButton;
+    [SerializeField] private Button registerButton;
+
+    private LoginController controller = new LoginController();
+
+    private void Start()
+    {
+        controller.Init();
+
+        loginButton.onClick.AddListener(() => controller.Login(emailField.text, passwordField.text));
+        registerButton.onClick.AddListener(() => controller.Register(emailField.text, passwordField.text));
+    }
+}
