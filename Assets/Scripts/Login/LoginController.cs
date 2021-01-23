3b6594835c8f07260a1d5b99553a77c2c7aeff6e
AmdHamdani <Hamdaniryuuken@gmail.com>
2021-01-23 14:31:08 +0700

    Add login controller

---
+++ Assets/Scripts/Login/LoginController.cs
@@ -0,0 +1,88 @@
+﻿using Firebase.Auth;
+using System;
+using UnityEngine;
+
+public class LoginController
+{
+
+    private FirebaseAuth auth;
+
+    public void Init()
+    {
+        auth = FirebaseAuth.DefaultInstance;
+        auth.StateChanged += OnStateChanged;
+    }
+
+    public void Register(string email, string password)
+    {
+        auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            // Firebase user has been created.
+            FirebaseUser newUser = task.Result;
+            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+
+            SendVerification();
+        });
+    }
+
+    public void Login(string email, string password)
+    {
+        auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            FirebaseUser newUser = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+    }
+
+    public void SendVerification()
+    {
+        FirebaseUser user = auth.CurrentUser;
+        if (user != null)
+        {
+            user.SendEmailVerificationAsync().ContinueWith(task => {
+                if (task.IsCanceled)
+                {
+                    Debug.LogError("SendEmailVerificationAsync was canceled.");
+                    return;
+                }
+                if (task.IsFaulted)
+                {
+                    Debug.LogError("SendEmailVerificationAsync encountered an error: " + task.Exception);
+                    return;
+                }
+
+                Debug.Log("Email sent successfully.");
+            });
+        }
+
+    }
+
+    private void OnStateChanged(object sender, EventArgs e)
+    {
+
+    }
+}
